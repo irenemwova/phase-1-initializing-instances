b8bc0a90589b42a4c239910c5b8dc9e916c6f4be
Irene Mwova <irene.mwova@student.moringaschool.com>
2024-04-30 11:18:08 +0300

    adds initializing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here
+// Write your code here// Create a class for Breakfast
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Create a class for Lunch
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Create a class for Dinner
+  class Dinner {
+    #dessert;
+    
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert;
+    }
+  }
+  
